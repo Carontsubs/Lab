1cda5a06193ac12d6c52819f05ae485e0c5d8cc6
Caront <carontsubs@gmail.com>
2026-02-04 09:19:54 +0100

    Ajustament del càlcul per dividir immediatament el volum final per 1000 en l'entrada.     Manteniment del càlcul del volum necessari (D) amb la conversió final a mil·lilitres.     Impressió del resultat amb un decimal per millorar la claredat de la sortida.

--- Calculs_Zincato.py
+++ Calculs_Zincato.py
@@ -2,10 +2,10 @@
 A = float (input ("Concetracio Zincato gr/l: "))
 E = float (input ("Concetracio Zn Original gr/l: "))
 C = float (input ("Concentracio final desitjada gr/l: "))
-B = float (input ("Volum final ml: "))
+B = float (input ("Volum final ml: ")) /1000
 
-D = ((C - E) / (A - E)) * (B/1000)
+D = ((C - E) / (A - E)) * B
 
 D = D *1000             #Converitr a mL
 
-print(f'{round(D)} mL')
+print(f'{round(D,1)} mL')
